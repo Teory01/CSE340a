44327fefd9f11f7145a319bdee427094c60fc7f4
teory01 <youremail@byupathway.edu>
2026-02-17 22:48:21 +0100

    fvvfv

---
+++ render.com.session.sql
@@ -0,0 +1,23 @@
+-- Create wishlist table
+CREATE TABLE IF NOT EXISTS public.wishlist (
+  wishlist_id SERIAL PRIMARY KEY,
+  account_id INT NOT NULL,
+  inv_id INT NOT NULL,
+  date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- One-to-many relationship: account → wishlist
+ALTER TABLE IF EXISTS public.wishlist
+  ADD CONSTRAINT fk_account
+  FOREIGN KEY (account_id)
+  REFERENCES public.account (account_id) MATCH SIMPLE
+  ON UPDATE CASCADE
+  ON DELETE NO ACTION;
+
+-- Inventory link
+ALTER TABLE IF EXISTS public.wishlist
+  ADD CONSTRAINT fk_inventory
+  FOREIGN KEY (inv_id)
+  REFERENCES public.inventory (inv_id) MATCH SIMPLE
+  ON UPDATE CASCADE
+  ON DELETE NO ACTION;
